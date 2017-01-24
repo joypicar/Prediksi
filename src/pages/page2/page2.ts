5893944fc85b1d2a6c211fbe59d9cfe9606cf913
SUPERGROUP\daniel.cinco <lidanielcinco@gmail.com>
2017-01-24 14:08:32 +0800

    page2 for standings

--- src/pages/page2/page2.ts
+++ src/pages/page2/page2.ts
@@ -1,38 +1,26 @@
 import { Component } from '@angular/core';
-
-import { NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-page2',
   templateUrl: 'page2.html'
 })
 export class Page2 {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
-  }
-
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(Page2, {
-      item: item
+  
+  posts: any;
+ 
+  constructor(public http: Http) {
+ 
+    this.http.get('http://prediksi-ligainggris.info/api/json_fixtures_API_mobile_standings.php').map(res => res.json()).subscribe(data => {
+        this.posts = data.results;
     });
+ 
   }
 }
+
+
+
+
+
+  
